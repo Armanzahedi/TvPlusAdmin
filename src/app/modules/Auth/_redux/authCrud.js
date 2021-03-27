83303a2b6c6eccf8fcad27bd081f37ee1d079144
Armanzdi <armanzahedi24@gmail.com>
2021-03-27 17:49:31 +0430

    added api login

--- src/app/modules/Auth/_redux/authCrud.js
+++ src/app/modules/Auth/_redux/authCrud.js
@@ -1,39 +1,43 @@
-import axios from "axios";
-var API_URL = process.env.REACT_APP_API_URL;
-var API_KEY = process.env.REACT_APP_API_KEY;
-export const LOGIN_URL = `/auth/login`;
-export const REGISTER_URL = `/auth/register`;
-export const REQUEST_PASSWORD_URL = `/auth/forgot-password`;
+import axios from "axios"
+var API_URL = process.env.REACT_APP_API_URL
+var API_KEY = process.env.REACT_APP_API_KEY
+export const LOGIN_URL = `/auth/login`
+export const REGISTER_URL = `/auth/register`
+export const REQUEST_PASSWORD_URL = `/auth/forgot-password`
 
-export const ME_URL = `/user/CurrentUser`;
-var authToken =   localStorage.getItem('authToken');
+export const ME_URL = `/auth/Me`
+var authToken = JSON.parse(localStorage.getItem("persist:auth"))
+if (authToken?.authToken != null) {
+  authToken = authToken.authToken.slice(1, -1)
+}
 const apiClient = axios.create({
   baseURL: API_URL,
-});
-apiClient.interceptors.request.use((config) => {
-  return ({
-    ...config,
-    headers: {
-      "ApiKey" : API_KEY,
-      'Authorization': `Bearer ${authToken}`
-  }
-  })
-},
-  error => Promise.reject(error),
-);
+})
+apiClient.interceptors.request.use(
+  (config) => {
+    return {
+      ...config,
+      headers: {
+        ApiKey: API_KEY,
+        Authorization: `Bearer ${authToken}`,
+      },
+    }
+  },
+  (error) => Promise.reject(error)
+)
 export function login(userName, password) {
-  return apiClient.post(LOGIN_URL, { userName, password });
+  return apiClient.post(LOGIN_URL, { userName, password })
 }
 
 export function register(email, fullname, username, password) {
-  return apiClient.post(REGISTER_URL, { email, fullname, username, password });
+  return apiClient.post(REGISTER_URL, { email, fullname, username, password })
 }
 
 export function requestPassword(email) {
-  return apiClient.post(REQUEST_PASSWORD_URL, { email });
+  return apiClient.post(REQUEST_PASSWORD_URL, { email })
 }
 
 export function getUserByToken() {
   // Authorization head should be fulfilled in interceptor.
-  return apiClient.get(ME_URL);
+  return apiClient.get(ME_URL)
 }
